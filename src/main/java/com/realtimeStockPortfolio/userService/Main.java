465735d58ecb70b1ac02cd675cb7a9b3a70c4724
iamsmt246 <sumit.developer24@gmail.com>
2024-11-24 11:35:19 -0500

    changing package name and adding user model class

--- src/main/java/com/realtimeStockPortfolio/userService/Main.java
+++ src/main/java/com/realtimeStockPortfolio/userService/Main.java
@@ -1,4 +1,4 @@
-package com.realtime_stock_portfolio;
+package com.realtimeStockPortfolio.userService;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
